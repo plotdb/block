1f421cd65ed646c5e3e0f054dcd453265f80495a
Kirby Wu <tkirby@gmail.com>
2021-12-09 14:58:51 +0800

    update test code for bnudle

--- web/src/ls/cache/index.ls
+++ web/src/ls/cache/index.ls
@@ -1,108 +1,112 @@
 <-(->it.apply {}) _
-window.blah = (cb) ~> @code = cb!
+[win, doc] = [window, window.document]
 tmp = ld$.find 'template', 0
 node = tmp.content
 
-_bundle = (list, blocks = [], deps = []) ->
-  if !list.length => return Promise.resolve {blocks, deps}
-  bd = list.splice 0, 1 .0
-  console.log 'block def: ', bd
-  ld$.fetch mgr.get-url(bd), {method: \GET}, {type: \text}
-    .then ->
-      node = document.createElement \div
-      node.innerHTML = it
-      if node.childNodes.length > 1 => console.warn "DOM definition of a block should contain only one root."
-      id = "#{bd.name}@#{bd.version}:#{bd.path or 'index.html'}"
-      js = Array.from(node.querySelectorAll \script)
-        .map ->
-          it.parentNode.removeChild it
-          it.textContent
-        .join \\n
-      css = Array.from(node.querySelectorAll \style)
-        .map ->
-          it.parentNode.removeChild it
-          it.textContent
-        .join \\n
-      node.childNodes.0.setAttribute \block, id
-      ret = eval(js) or {}
-      if ret.{}pkg.extend => list.push ret.{}pkg.extend
-      deps ++= (ret.{}pkg.dependencies or []).filter -> it.type == \js or /\.js/.exec((it.path or it or ''))
-      blocks.push {js, css, html: node.innerHTML, bd, id}
-      return _bundle list, blocks, deps
+/*
+fold =
+  bundle: (opt = {}) ->
+    mgr = opt.manager or @ 
+    _ = (list, blocks = [], deps = {js: [], css: []}) ->
+      if !list.length => return Promise.resolve {blocks, deps}
+      bd = list.splice 0, 1 .0
+      ld$.fetch mgr.get-url(bd), {method: \GET}, {type: \text}
+        .then ->
+          node = doc.createElement \div
+          node.innerHTML = it
+          if node.childNodes.length > 1 => console.warn "DOM definition of a block should contain only one root."
+          id = "#{bd.name}@#{bd.version}:#{bd.path or 'index.html'}"
+          [js,css] = <[script style]>.map (n)-> 
+            Array.from(node.querySelectorAll n)
+              .map -> it.parentNode.removeChild(it); it.textContent
+              .join \\n
+          node.childNodes.0.setAttribute \block, id
+          ret = eval(js) or {}
+          if ret.{}pkg.extend => list.push ret.{}pkg.extend
+          deps.js ++= (ret.{}pkg.dependencies or []).filter -> it.type == \js or /\.js/.exec((it.path or it or ''))
+          deps.css ++= (ret.{}pkg.dependencies or []).filter -> it.type == \css or /\.css/.exec((it.path or it or ''))
+          blocks.push {js, css, html: node.innerHTML, bd, id}
+          return _ list, blocks, deps
+    _ opt.[]blocks
+      .then ({blocks, deps}) ->
+        Promise.all [
+          mgr.csscope.bundle(deps.css),
+          mgr.rescope.bundle(deps.js)
+        ]
+          .then ([depcss, depjs]) ->
+            console.log "/", depcss, "/", depjs
+            js = blocks.map (b) -> "\"#{b.id}\": #{(b.js or '""').replace(/;$/,'')}"
+            js = "document.currentScript.import({#{js.join(',\n')}});"
+            css = blocks
+              .map (b) ->
+                scope = csscope.scope b
+                csscope {rule: "*[scope~=#{scope}]", name: scope, css: (b.css or ''), scope-test: "[scope]"}
+              .join \\n
+            html = blocks.map(-> it.html or '').join(\\n)
+            return """
+            <template>
+              #html
+              <style type="text/css">#css#depcss</style>
+              <script type="text/javascript">#js#depjs</script>
+            </template>
+            """
 
-bundle = (opt = {}) ->
-  mgr = opt.manager
-  _bundle opt.[]blocks
-    .then ({blocks, deps}) ->
-      mgr.rescope.bundle deps
-        .then (libs) ->
-          js = blocks
-            .map (b) ->
-              "\"#{b.id}\": #{(b.js or '""').replace(/;$/,'')}"
-            .join ",\n"
-          js = "document.currentScript.import({#js});"
-          css = blocks
-            .map (b) ->
-              scope = ('_' + btoa(b).replace(/=/g,'_'))
-              csscope {rule: "*[scope~=#{scope}]", name: scope, css: (b.css or ''), scope-test: "[scope]"}
-            .join \\n
-          html = blocks.map(-> it.html or '').join(\\n)
-          ret = """
-          <template>
-            #html
-            <style type="text/css">#css</style>
-            <script type="text/javascript">#js#libs</script>
-          </template>
-          """
-        .then -> it
-
-parse-bundle = (opt = {}) ->
-  @root = root = if typeof(opt.root) == \string => document.querySelector(opt.root) else opt.root
-  if root.content => @root = root = root.content
-  @ <<< nodes: {}, classes: {}, codes: {}, mgr: opt.manager
-  @nodes = {}
-  @classes = {}
-  Array.from(root.childNodes).map (n) ~>
-    if n.nodeType != Document.ELEMENT_NODE => return
-    if n.nodeName == \SCRIPT => @script = n.cloneNode true
-    else if n.nodeName == \STYLE => @style = n.cloneNode true
-    else if !(id = n.getAttribute(\block)) => return
-    else @nodes[id] = n
-  if @script =>
-    scr = document.createElement \script
-    scr.textContent = @script.textContent
-    @script = scr
-    @script.import = ~> @codes = it
-    @script.setAttribute \type, \text/javascript
-    document.body.appendChild @script
-  if @style =>
-    @style.setAttribute \type, \text/css
-    document.body.appendChild @style
-  for k,node of @nodes =>
-    ret = /^(@?[^@]+)@([^:]+)(:.+)?/.exec(k)
-    [name, version, path] = [ret.1, ret.2, ((ret.3 or '').replace(/^:/,'') or '')]
-    @classes[k] = bc = new block.class {
-      manager: @mgr, name: name, version: version, path: path,
-      code: script: @codes[k], dom: node, style: ""
-    }
-    @mgr.set bc
-    
-  @
+  debundle: (opt = {}) ->
+    mgr = opt.manager or @
+    lc = {}
+    if !opt.root =>
+      p = if opt.url => ld$.fetch opt.url, {method: \GET}, {type: \text}
+      else Promise.resolve(opt.code)
+      p = p.then (c) ->
+        if !block.debundle-node => document.body.appendChild block.debundle-node = doc.createElement \div
+        block.debundle-node.appendChild(div = doc.createElement \div)
+        div.innerHTML = c
+        div.querySelector('template')
+    else p = Promise.resolve( if typeof(opt.root) == \string => doc.querySelector(opt.root) else opt.root )
+    p.then (root) ->
+      if root.content => root = root.content
+      [nodes, classes] = [{}, {}]
+      Array.from(root.childNodes).map (n) ~>
+        if n.nodeType != doc.ELEMENT_NODE => return
+        if n.nodeName == \SCRIPT => lc.script = n.cloneNode true
+        else if n.nodeName == \STYLE => lc.style = n.cloneNode true
+        else if !(id = n.getAttribute(\block)) => return
+        else nodes[id] = n
+      if lc.script =>
+        # needed if lc.script is loaded from fetch + innerHTML
+        s = doc.createElement \script
+        s.textContent = lc.script.textContent
+        lc.script = s
+        lc.script.import = ~> lc.codes = it
+        lc.script.setAttribute \type, \text/javascript
+        doc.body.appendChild lc.script
+      if lc.style =>
+        lc.style.setAttribute \type, \text/css
+        doc.body.appendChild lc.style
+      for k,node of nodes =>
+        ret = /^(@?[^@]+)@([^:]+)(:.+)?/.exec(k)
+        [name, version, path] = [ret.1, ret.2, ((ret.3 or '').replace(/^:/,'') or '')]
+        bc = new block.class {
+          manager: mgr, name: name, version: version, path: path,
+          code: script: lc.codes[k], dom: node, style: ""
+        }
+        console.log bc
+        mgr.set bc
+*/
 
 mgr = new block.manager registry: ({name, version, path, type}) -> "/block/#name/#version/#{path or 'index.html'}"
 
 if false =>
-  bundle {manager: mgr, blocks: [
+  mgr.bundle {blocks: [
     * name: 'cta', version: '0.0.1'
     * name: 'long-answer', version: '0.0.1'
     * name: 'sample', version: '0.0.1'
   ]}
     .then ->
-      ldfile.download data: it, name: "bundle-2.html"
+      ldfile.download data: it, name: "bundle-4.html"
 
 
 
-obj = parse-bundle.call {}, {root: ld$.find('template',0), manager: mgr}
 
 testload = (bd) ->
   mgr.get bd
@@ -111,25 +115,23 @@ testload = (bd) ->
         bi.attach {root: container}
           .then -> bi.interface!
 
-
-mgr.get {name: "@plotdb/sample1", version: "main"}
-  .then (bc) ->
-    bc.create!then (bi) ->
-      bi.attach {root: container}
-        .then -> bi.interface!
-
-mgr.get {name: "@plotdb/sample2", version: "main"}
-  .then (bc) ->
-    bc.create!then (bi) ->
-      bi.attach {root: container}
-        .then -> bi.interface!
-
-ld$.fetch "/assets/bundle/bundle-2.html", {method: \GET}, {type: \text}
+#obj = parse-bundle.call {}, {root: ld$.find('template',0), manager: mgr}
+mgr.debundle root: ld$.find('template', 0)
   .then ->
-    div = document.createElement \div
-    div.innerHTML = it
-    document.body.appendChild div
-    obj = parse-bundle.call {}, {root: ld$.find(div, 'template',0), manager: mgr}
-  .then ->
-    testload {name: "cta", version: "0.0.1", path: "index.html"}
-    testload {name: "long-answer", version: "0.0.1", path: "index.html"}
+
+    mgr.get {name: "@plotdb/sample1", version: "main"}
+      .then (bc) ->
+        bc.create!then (bi) ->
+          bi.attach {root: container}
+            .then -> bi.interface!
+    mgr.get {name: "@plotdb/sample2", version: "main"}
+      .then (bc) ->
+        bc.create!then (bi) ->
+          bi.attach {root: container}
+            .then -> bi.interface!
+
+if true =>
+  mgr.debundle url: "/assets/files/cache/bundled-blocks.html"
+    .then ->
+      testload {name: "cta", version: "0.0.1", path: "index.html"}
+      testload {name: "long-answer", version: "0.0.1", path: "index.html"}
--- web/static/js/cache/index.js
+++ web/static/js/cache/index.js
@@ -1,155 +1,100 @@
 (function(it){
   return it.apply({});
 })(function(){
-  var tmp, node, _bundle, bundle, parseBundle, mgr, obj, testload, this$ = this;
-  window.blah = function(cb){
-    return this$.code = cb();
-  };
+  var ref$, win, doc, tmp, node, mgr, testload;
+  ref$ = [window, window.document], win = ref$[0], doc = ref$[1];
   tmp = ld$.find('template', 0);
   node = tmp.content;
-  _bundle = function(list, blocks, deps){
-    var bd;
-    blocks == null && (blocks = []);
-    deps == null && (deps = []);
-    if (!list.length) {
-      return Promise.resolve({
-        blocks: blocks,
-        deps: deps
-      });
-    }
-    bd = list.splice(0, 1)[0];
-    console.log('block def: ', bd);
-    return ld$.fetch(mgr.getUrl(bd), {
-      method: 'GET'
-    }, {
-      type: 'text'
-    }).then(function(it){
-      var node, id, js, css, ret;
-      node = document.createElement('div');
-      node.innerHTML = it;
-      if (node.childNodes.length > 1) {
-        console.warn("DOM definition of a block should contain only one root.");
-      }
-      id = bd.name + "@" + bd.version + ":" + (bd.path || 'index.html');
-      js = Array.from(node.querySelectorAll('script')).map(function(it){
-        it.parentNode.removeChild(it);
-        return it.textContent;
-      }).join('\n');
-      css = Array.from(node.querySelectorAll('style')).map(function(it){
-        it.parentNode.removeChild(it);
-        return it.textContent;
-      }).join('\n');
-      node.childNodes[0].setAttribute('block', id);
-      ret = eval(js) || {};
-      if ((ret.pkg || (ret.pkg = {})).extend) {
-        list.push((ret.pkg || (ret.pkg = {})).extend);
-      }
-      deps = deps.concat(((ret.pkg || (ret.pkg = {})).dependencies || []).filter(function(it){
-        return it.type === 'js' || /\.js/.exec(it.path || it || '');
-      }));
-      blocks.push({
-        js: js,
-        css: css,
-        html: node.innerHTML,
-        bd: bd,
-        id: id
-      });
-      return _bundle(list, blocks, deps);
-    });
-  };
-  bundle = function(opt){
-    var mgr;
-    opt == null && (opt = {});
-    mgr = opt.manager;
-    return _bundle(opt.blocks || (opt.blocks = [])).then(function(arg$){
-      var blocks, deps;
-      blocks = arg$.blocks, deps = arg$.deps;
-      return mgr.rescope.bundle(deps).then(function(libs){
-        var js, css, html, ret;
-        js = blocks.map(function(b){
-          return "\"" + b.id + "\": " + (b.js || '""').replace(/;$/, '');
-        }).join(",\n");
-        js = "document.currentScript.import({" + js + "});";
-        css = blocks.map(function(b){
-          var scope;
-          scope = '_' + btoa(b).replace(/=/g, '_');
-          return csscope({
-            rule: "*[scope~=" + scope + "]",
-            name: scope,
-            css: b.css || '',
-            scopeTest: "[scope]"
-          });
-        }).join('\n');
-        html = blocks.map(function(it){
-          return it.html || '';
-        }).join('\n');
-        return ret = "<template>\n  " + html + "\n  <style type=\"text/css\">" + css + "</style>\n  <script type=\"text/javascript\">" + js + libs + "</script>\n</template>";
-      }).then(function(it){
-        return it;
-      });
-    });
-  };
-  parseBundle = function(opt){
-    var root, scr, k, ref$, node, ret, ref1$, name, version, path, bc, this$ = this;
-    opt == null && (opt = {});
-    this.root = root = typeof opt.root === 'string'
-      ? document.querySelector(opt.root)
-      : opt.root;
-    if (root.content) {
-      this.root = root = root.content;
-    }
-    this.nodes = {};
-    this.classes = {};
-    this.codes = {};
-    this.mgr = opt.manager;
-    this.nodes = {};
-    this.classes = {};
-    Array.from(root.childNodes).map(function(n){
-      var id;
-      if (n.nodeType !== Document.ELEMENT_NODE) {
-        return;
-      }
-      if (n.nodeName === 'SCRIPT') {
-        return this$.script = n.cloneNode(true);
-      } else if (n.nodeName === 'STYLE') {
-        return this$.style = n.cloneNode(true);
-      } else if (!(id = n.getAttribute('block'))) {} else {
-        return this$.nodes[id] = n;
-      }
-    });
-    if (this.script) {
-      scr = document.createElement('script');
-      scr.textContent = this.script.textContent;
-      this.script = scr;
-      this.script['import'] = function(it){
-        return this$.codes = it;
-      };
-      this.script.setAttribute('type', 'text/javascript');
-      document.body.appendChild(this.script);
-    }
-    if (this.style) {
-      this.style.setAttribute('type', 'text/css');
-      document.body.appendChild(this.style);
-    }
-    for (k in ref$ = this.nodes) {
-      node = ref$[k];
-      ret = /^(@?[^@]+)@([^:]+)(:.+)?/.exec(k);
-      ref1$ = [ret[1], ret[2], (ret[3] || '').replace(/^:/, '') || ''], name = ref1$[0], version = ref1$[1], path = ref1$[2];
-      this.classes[k] = bc = new block['class']({
-        manager: this.mgr,
-        name: name,
-        version: version,
-        path: path,
-        code: {
-          script: this.codes[k],
-          dom: node,
-          style: ""
-        }
-      });
-      this.mgr.set(bc);
-    }
-    return this;
-  };
+  /*
+  fold =
+    bundle: (opt = {}) ->
+      mgr = opt.manager or @ 
+      _ = (list, blocks = [], deps = {js: [], css: []}) ->
+        if !list.length => return Promise.resolve {blocks, deps}
+        bd = list.splice 0, 1 .0
+        ld$.fetch mgr.get-url(bd), {method: \GET}, {type: \text}
+          .then ->
+            node = doc.createElement \div
+            node.innerHTML = it
+            if node.childNodes.length > 1 => console.warn "DOM definition of a block should contain only one root."
+            id = "#{bd.name}@#{bd.version}:#{bd.path or 'index.html'}"
+            [js,css] = <[script style]>.map (n)-> 
+              Array.from(node.querySelectorAll n)
+                .map -> it.parentNode.removeChild(it); it.textContent
+                .join \\n
+            node.childNodes.0.setAttribute \block, id
+            ret = eval(js) or {}
+            if ret.{}pkg.extend => list.push ret.{}pkg.extend
+            deps.js ++= (ret.{}pkg.dependencies or []).filter -> it.type == \js or /\.js/.exec((it.path or it or ''))
+            deps.css ++= (ret.{}pkg.dependencies or []).filter -> it.type == \css or /\.css/.exec((it.path or it or ''))
+            blocks.push {js, css, html: node.innerHTML, bd, id}
+            return _ list, blocks, deps
+      _ opt.[]blocks
+        .then ({blocks, deps}) ->
+          Promise.all [
+            mgr.csscope.bundle(deps.css),
+            mgr.rescope.bundle(deps.js)
+          ]
+            .then ([depcss, depjs]) ->
+              console.log "/", depcss, "/", depjs
+              js = blocks.map (b) -> "\"#{b.id}\": #{(b.js or '""').replace(/;$/,'')}"
+              js = "document.currentScript.import({#{js.join(',\n')}});"
+              css = blocks
+                .map (b) ->
+                  scope = csscope.scope b
+                  csscope {rule: "*[scope~=#{scope}]", name: scope, css: (b.css or ''), scope-test: "[scope]"}
+                .join \\n
+              html = blocks.map(-> it.html or '').join(\\n)
+              return """
+              <template>
+                #html
+                <style type="text/css">#css#depcss</style>
+                <script type="text/javascript">#js#depjs</script>
+              </template>
+              """
+  
+    debundle: (opt = {}) ->
+      mgr = opt.manager or @
+      lc = {}
+      if !opt.root =>
+        p = if opt.url => ld$.fetch opt.url, {method: \GET}, {type: \text}
+        else Promise.resolve(opt.code)
+        p = p.then (c) ->
+          if !block.debundle-node => document.body.appendChild block.debundle-node = doc.createElement \div
+          block.debundle-node.appendChild(div = doc.createElement \div)
+          div.innerHTML = c
+          div.querySelector('template')
+      else p = Promise.resolve( if typeof(opt.root) == \string => doc.querySelector(opt.root) else opt.root )
+      p.then (root) ->
+        if root.content => root = root.content
+        [nodes, classes] = [{}, {}]
+        Array.from(root.childNodes).map (n) ~>
+          if n.nodeType != doc.ELEMENT_NODE => return
+          if n.nodeName == \SCRIPT => lc.script = n.cloneNode true
+          else if n.nodeName == \STYLE => lc.style = n.cloneNode true
+          else if !(id = n.getAttribute(\block)) => return
+          else nodes[id] = n
+        if lc.script =>
+          # needed if lc.script is loaded from fetch + innerHTML
+          s = doc.createElement \script
+          s.textContent = lc.script.textContent
+          lc.script = s
+          lc.script.import = ~> lc.codes = it
+          lc.script.setAttribute \type, \text/javascript
+          doc.body.appendChild lc.script
+        if lc.style =>
+          lc.style.setAttribute \type, \text/css
+          doc.body.appendChild lc.style
+        for k,node of nodes =>
+          ret = /^(@?[^@]+)@([^:]+)(:.+)?/.exec(k)
+          [name, version, path] = [ret.1, ret.2, ((ret.3 or '').replace(/^:/,'') or '')]
+          bc = new block.class {
+            manager: mgr, name: name, version: version, path: path,
+            code: script: lc.codes[k], dom: node, style: ""
+          }
+          console.log bc
+          mgr.set bc
+  */
   mgr = new block.manager({
     registry: function(arg$){
       var name, version, path, type;
@@ -158,8 +103,7 @@
     }
   });
   if (false) {
-    bundle({
-      manager: mgr,
+    mgr.bundle({
       blocks: [
         {
           name: 'cta',
@@ -175,14 +119,10 @@
     }).then(function(it){
       return ldfile.download({
         data: it,
-        name: "bundle-2.html"
+        name: "bundle-4.html"
       });
     });
   }
-  obj = parseBundle.call({}, {
-    root: ld$.find('template', 0),
-    manager: mgr
-  });
   testload = function(bd){
     return mgr.get(bd).then(function(bc){
       return bc.create().then(function(bi){
@@ -194,53 +134,48 @@
       });
     });
   };
-  mgr.get({
-    name: "@plotdb/sample1",
-    version: "main"
-  }).then(function(bc){
-    return bc.create().then(function(bi){
-      return bi.attach({
-        root: container
-      }).then(function(){
-        return bi['interface']();
-      });
-    });
-  });
-  mgr.get({
-    name: "@plotdb/sample2",
-    version: "main"
-  }).then(function(bc){
-    return bc.create().then(function(bi){
-      return bi.attach({
-        root: container
-      }).then(function(){
-        return bi['interface']();
-      });
-    });
-  });
-  return ld$.fetch("/assets/bundle/bundle-2.html", {
-    method: 'GET'
-  }, {
-    type: 'text'
-  }).then(function(it){
-    var div, obj;
-    div = document.createElement('div');
-    div.innerHTML = it;
-    document.body.appendChild(div);
-    return obj = parseBundle.call({}, {
-      root: ld$.find(div, 'template', 0),
-      manager: mgr
-    });
+  mgr.debundle({
+    root: ld$.find('template', 0)
   }).then(function(){
-    testload({
-      name: "cta",
-      version: "0.0.1",
-      path: "index.html"
+    mgr.get({
+      name: "@plotdb/sample1",
+      version: "main"
+    }).then(function(bc){
+      return bc.create().then(function(bi){
+        return bi.attach({
+          root: container
+        }).then(function(){
+          return bi['interface']();
+        });
+      });
     });
-    return testload({
-      name: "long-answer",
-      version: "0.0.1",
-      path: "index.html"
+    return mgr.get({
+      name: "@plotdb/sample2",
+      version: "main"
+    }).then(function(bc){
+      return bc.create().then(function(bi){
+        return bi.attach({
+          root: container
+        }).then(function(){
+          return bi['interface']();
+        });
+      });
     });
   });
+  if (true) {
+    return mgr.debundle({
+      url: "/assets/files/cache/bundled-blocks.html"
+    }).then(function(){
+      testload({
+        name: "cta",
+        version: "0.0.1",
+        path: "index.html"
+      });
+      return testload({
+        name: "long-answer",
+        version: "0.0.1",
+        path: "index.html"
+      });
+    });
+  }
 });
